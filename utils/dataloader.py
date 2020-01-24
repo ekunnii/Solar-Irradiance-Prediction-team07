4a2fa9e46b5288dca529144578c256f21bf2f9d2
Number One Hundred Thirty Six Guest <guest136@helios3.helios.calculquebec.ca>
2020-01-24 19:45:17 +0000

    initial file of dataloader

---
+++ utils/dataloader.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+import pathlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+np.set_printoptions(precision=4)
+
+if __name__ == "__main__":
+    pass
