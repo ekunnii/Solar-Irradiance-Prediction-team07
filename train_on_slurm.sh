f5c96995457940d8a94e2d1f03f3dc19fa90d226
Number One Hundred Thirty Six Guest <guest136@helios3.helios.calculquebec.ca>
2020-02-07 22:03:18 +0000

    fix pip install errors

--- train_on_slurm.sh
+++ train_on_slurm.sh
@@ -6,8 +6,22 @@
 
 module load python/3.7
 virtualenv --no-download $SLURM_TMPDIR/env
+echo $SLURM_TMPDIR
 source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index tensorflow_gpu==2
 
-python ./train.py $PWD+"/train_config.json" --scratch_dir $SCRATCH 
+
+pip install --no-index tensorflow_gpu==2
+pip install --no-index pandas
+pip install /project/cq-training-1/project1/teams/team07/lz4-3.0.2-cp37-cp37m-linux_x86_64.whl
+pip install --no-index opencv-python
+pip install --no-index matplotlib
+pip install --no-index tqdm
+
+# python --version
+# which python
+# which pip
+# pip freeze
+
+python ./train.py $PWD"/train_config.json" --scratch_dir $SCRATCH 
 python ./test.py
+
