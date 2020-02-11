9726cfd3ccc54249c4b4ada4c729b7641a7f34e6
MaximeDaigle <maximedaigle95@gmail.com>
2020-02-11 11:48:18 -0500

    Update dataset.py

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -115,14 +115,55 @@ def BuildDataSet(
                         t_0 = row.name
                         station_ghis = []
                         for offset in target_time_offsets:
-                            # remove negative value
-                            station_ghis.append(round(max(dataframe.loc[t_0 + offset][station_idx + "_GHI"],0),2))
+
+                            if np.isnan(dataframe.loc[t_0 + offset][station_idx + "_GHI"]):
+                                station_ghis.append(
+                                    round(max(dataframe.loc[t_0 + offset][station_idx + "_CLEARSKY_GHI"], 0), 2))
+
+                                # ##DEBUG
+                                # # Try to extrapolate with close available ghi when possible and clearsky when its not
+                                # # However, ghi seems to be always unavailable in big gaps.
+                                # # So changed it so it's always using clearsky
+
+                                # offset_15min = pd.Timedelta("P0DT0H15M0S", ).to_pytimedelta()
+                                # ghi_just_before = dataframe.loc[t_0 + offset - offset_15min][station_idx + "_GHI"]
+                                # ghi_just_after = dataframe.loc[t_0 + offset + offset_15min][station_idx + "_GHI"]
+                                # if np.isnan(ghi_just_before) or np.isnan(ghi_just_after):
+                                #     ghi_just_before = dataframe.loc[t_0 + offset - (offset_15min*2)][station_idx + "_GHI"]
+                                #     ghi_just_after = dataframe.loc[t_0 + offset + (offset_15min*2)][station_idx + "_GHI"]
+                                # # ghi is not available for a gap of time, then use clearsky
+                                # if np.isnan(ghi_just_before) or np.isnan(ghi_just_after):
+                                #     print('clearsky', station_idx,dataframe.loc[t_0 + offset][station_idx + "_GHI"], ghi_just_before, ghi_just_after, dataframe.loc[t_0 + offset][station_idx + "_CLEARSKY_GHI"])
+                                #     station_ghis.append(
+                                #         round(max(dataframe.loc[t_0 + offset][station_idx + "_CLEARSKY_GHI"], 0), 2))
+                                # else:
+                                #     print('extrapolate',station_idx,dataframe.loc[t_0 + offset][station_idx + "_GHI"], ghi_just_before, ghi_just_after, (ghi_just_before+ghi_just_after)/2)
+                                #     station_ghis.append(
+                                #         round(max((ghi_just_before+ghi_just_before)/2, 0), 2))
+                                # ##DEBUG
+                            else:
+                                # ##DEBUG
+                                # jump_day = pd.Timedelta("P1DT0H0M0S", ).to_pytimedelta()
+                                # if (t_0 + offset - jump_day) in dataframe.index and (t_0 + offset + jump_day) in dataframe.index :
+                                #     day_before = dataframe.loc[t_0 + offset - jump_day][station_idx + "_GHI"]
+                                #     day_after = dataframe.loc[t_0 + offset + jump_day][station_idx + "_GHI"]
+                                #
+                                #     offset_15min = pd.Timedelta("P0DT0H15M0S", ).to_pytimedelta()
+                                #     ghi_just_before = dataframe.loc[t_0 + offset - offset_15min][station_idx + "_GHI"]
+                                #     ghi_just_after = dataframe.loc[t_0 + offset + offset_15min][station_idx + "_GHI"]
+                                #     print('ghi',station_idx,dataframe.loc[t_0 + offset][station_idx + "_GHI"], 'clearsky', dataframe.loc[t_0 + offset][station_idx + "_CLEARSKY_GHI"], 'days ext', (day_before+day_after)/2, 'neighbor extr', (ghi_just_before+ghi_just_after)/2)
+                                # ##DEBUG
+
+                                # change negative ghi to 0 and round it
+                                station_ghis.append(round(max(dataframe.loc[t_0 + offset][station_idx + "_GHI"],0),2))
                         
                         yield (meta_array, image_data, station_ghis)
     # End of generator
 
     # ## debugging
-    # next(_train_dataset())
+    # #next(_train_dataset())
+    # for i in _train_dataset():
+    #     print()
     # ## debugging
 
     image_shape = (image_dim[0], image_dim[1], len(channels))
@@ -151,15 +192,29 @@ class TrainingDataSet(tf.data.Dataset):
         data_frame = pd.read_pickle(fast_data_frame_path)
 
         # ## debug
-        # data_frame = data_frame[data_frame.index >= datetime.datetime.fromisoformat('2010-05-01 08:00:00')]
+        # # data_frame = data_frame[data_frame.index >= datetime.datetime.fromisoformat('2010-05-01 08:00:00')]
+        # data_frame = data_frame[data_frame.index >= datetime.datetime.fromisoformat('2010-05-01 13:45:00')]
+        # # data_frame = data_frame[data_frame.index >= datetime.datetime.fromisoformat('2010-05-04 01:15:00')]
+        # # print(data_frame["BND_GHI"].value_counts())
+        # # print(data_frame["BND_GHI"].describe())
+        # # print(data_frame["TBL_GHI"].isna().sum())
+        # # t = ["BND","TBL","DRA","FPK","GWN","PSU", "SXF"]
+        # # for e in t:
+        # #     data_frame = data_frame.drop(t + "_GHI",1)
+        # print(data_frame.iloc[:,5:][pd.isna(data_frame.iloc[:,5:]).any(axis=1)].head())
+        # # print(np.isnan(data_frame.loc[datetime.datetime.fromisoformat('2010-05-01 14:15:00')]["PSU_GHI"]))
         # ## debug
 
         target_time_offsets = [pd.Timedelta(d).to_pytimedelta() for d in admin_config["target_time_offsets"]]
             
         return BuildDataSet(data_frame, stations, target_time_offsets, user_config)
 
-
+# ## debug
 # if __name__ == "__main__":
+#     pd.set_option('display.max_columns', None)  # or 1000
+#     pd.set_option('display.max_rows', None)  # or 1000
+#     pd.set_option('display.max_colwidth', None)  # or 199
+#
 #     dataframe_path = "../data/catalog.helios.public.20100101-20160101_updated.pkl"
 #     stations = {
 #         "BND": [40.05192, -88.37309, 230],
@@ -175,3 +230,4 @@ class TrainingDataSet(tf.data.Dataset):
 #         admin_json = json.load(tc)
 #
 #     data = TrainingDataSet(dataframe_path, stations, admin_json)
+# ## debug
